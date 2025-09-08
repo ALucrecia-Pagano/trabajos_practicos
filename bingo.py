fd47ea46752678572f3df00b6c6fa1625d36a9a7
amanda pagano <amandapagano05@gmail.com>
2025-09-08 10:48:46 -0300

    bingo trabajo en clase

---
+++ bingo.py
@@ -0,0 +1,33 @@
+import random
+
+num= random.sample(range(1,51), 25)
+carton = [num[i:i+5]for i in range(0,25,5)]
+
+bolillero= list(range(1,51))
+random.shuffle(bolillero)
+
+while True:
+    print("\n presiona enter para sacar un numero")
+    nro= bolillero.pop()
+    encontrado = False
+    for i in range(5):
+        for j in range(5):
+            if carton[i][j]== nro:
+                carton[i][j]= 0
+                encontrado= True
+    if encontrado:
+        print(f"se sorteo el numero: {nro} ------ Tu lo tienes en tu carton!!")
+    else:
+        print(f"se sorteo el numero: {nro}---- No lo tienes en tu carton!")
+    for fila in carton:
+        print(fila)
+
+    if any(all(x == 0 for x in fila) for fila in carton):
+        print("Linea!!")
+
+    if any(all(x == 0 for x in fila) for fila in carton):
+        print("¡¡BINGO!!")
+        break        
+
+
+
